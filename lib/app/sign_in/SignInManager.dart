960ed7018a71ffe85b99edb2ab4a4c8e8933175c
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-02-24 19:09:22 +0530

    259 diff b/n Bloc & Change

--- lib/app/sign_in/SignInManager.dart
+++ lib/app/sign_in/SignInManager.dart
@@ -4,20 +4,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class SignInBloc {
-  SignInBloc({required this.auth, required this.isLoading});
+class SignInManager {
+  SignInManager({required this.auth, required this.isLoading});
 
   final AuthBase auth;
   final ValueNotifier<bool> isLoading;
-  //
-  // final StreamController<bool> _isLoadingController = StreamController<bool>();
-  //
-  // Stream<bool> get isLoadingStream => _isLoadingController.stream;
-  //
-  // void dispose() {
-  //   _isLoadingController.close();
-  // }
-  // void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
 
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
