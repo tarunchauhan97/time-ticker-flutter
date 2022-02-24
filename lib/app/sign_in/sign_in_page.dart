552c99c940f55445f4a315b99648a8ba61bf6195
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-02-24 17:12:16 +0530

    258 consumer notified explained

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -10,8 +10,9 @@ import 'package:time_tracker_flutter_course/common_widgets/show_exception_alert_
 import '../../services/auth.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({Key? key, required this.bloc}) : super(key: key);
+  const SignInPage({Key? key, required this.bloc, required this.isLoading}) : super(key: key);
   final SignInBloc bloc;
+  final bool isLoading;
 
   static Widget create(BuildContext context) {
     final auth = Provider.of<AuthBase>(context, listen: false);
@@ -20,7 +21,9 @@ class SignInPage extends StatelessWidget {
       child: Consumer<ValueNotifier<bool>>(
         builder: (_, isLoading, __) => Provider<SignInBloc>(
           create: (_) => SignInBloc(auth: auth, isLoading: isLoading),
-          child: Consumer<SignInBloc>(builder: (_, bloc, __) => SignInPage(bloc: bloc)),
+          child: Consumer<SignInBloc>(
+            builder: (_, bloc, __) => SignInPage(bloc: bloc, isLoading: isLoading.value),
+          ),
         ),
       ),
     );
@@ -63,26 +66,28 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final bloc = Provider.of<SignInBloc>(context, listen: false);
-    final isLoading = Provider.of<ValueNotifier<bool>>(context);
+    //final isLoading = Provider.of<ValueNotifier<bool>>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: _buildContent(context, isLoading.value),
+      body: _buildContent(context),
       backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget _buildContent(BuildContext context, bool isLoading) {
+  Widget _buildContent(
+    BuildContext context,
+  ) {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          SizedBox(height: 50, child: _buildHeader(isLoading)),
+          SizedBox(height: 50, child: _buildHeader()),
           SizedBox(height: 48.0),
           SocialSignInButton(
             assetName: 'images/google-logo.png',
@@ -125,7 +130,7 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  Widget _buildHeader(bool isLoading) {
+  Widget _buildHeader() {
     if (isLoading) {
       return Center(
         child: CircularProgressIndicator(),
