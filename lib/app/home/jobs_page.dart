7024faa2bc3c51e091f0ebfac05115e68471c62a
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-02-25 13:25:57 +0530

    280 Writing data to firestore

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
 
 import '../../services/auth.dart';
 
@@ -27,6 +28,14 @@ class JobsPage extends StatelessWidget {
     }
   }
 
+  void _createJob(BuildContext context) async {
+    final database = Provider.of<Database>(context, listen: false);
+    await database.createJob({
+      'name': 'Blogging',
+      'ratePerHour': 10,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +53,10 @@ class JobsPage extends StatelessWidget {
             onPressed: () => _confirmSignOut(context),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createJob(context),
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,17 @@
-abstract class Database {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class Database {
+  Future<void> createJob(Map<String, dynamic> jobData);
+}
 
 class FireStoreDataBase implements Database {
   FireStoreDataBase({required this.uid}) : assert(uid != null);
 
   final String uid;
+
+  Future<void> createJob(Map<String, dynamic> jobData) async {
+    final path = '/users/$uid/jobs/job_abc';
+    final documentReference = FirebaseFirestore.instance.doc(path);
+    await documentReference.set(jobData);
+  }
 }
