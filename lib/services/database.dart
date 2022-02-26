04f09cf3a0bbef90b9fb7964e2969dcd5f613ccd
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-02-26 06:27:47 +0530

    283 geneic setData Method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -11,9 +11,20 @@ class FireStoreDataBase implements Database {
 
   final String uid;
 
-  Future<void> createJob(Job job) async {
-    final path = APIPath.job(uid, 'job_abc');
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(job.toMap());
+  Future<void> createJob(Job job) => _setData(
+        path: APIPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
+
+  Future<void> _setData({required String path, required Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('---------$path:$data---------');
+    await reference.set(data);
   }
+
+// Future<void> createJob(Job job) async {
+//   final path = APIPath.job(uid, 'job_abc');
+//   final documentReference = FirebaseFirestore.instance.doc(path);
+//   await documentReference.set(job.toMap());
+// }
 }
