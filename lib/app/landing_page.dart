c574207c174f71471b148767a369cb56fe96fbef
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-03 06:59:24 +0530

    tp-googlsignin

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,42 +4,24 @@ import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   const LandingPage({Key? key, required this.auth}) : super(key: key);
 
   final AuthBase auth;
 
-  @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  User? _user;
-
-  @override
-  void initState() {
-    super.initState();
-    print(widget.auth.currentUser);
-    _updateUser(widget.auth.currentUser);
-  }
-
-  void _updateUser(User? user) {
-    setState(() {
-      _user = user;
-    });
-  }
+  void _updateUser(User? user) {}
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: widget.auth.authStateChanges(),
+      stream: auth.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final User? user = snapshot.data;
           if (user == null) {
-            return SignInPage(auth: widget.auth, onSignIn: _updateUser);
+            return SignInPage(auth: auth);
           } else {
-            return HomePage(onSignOut: () => _updateUser(null), auth: widget.auth);
+            return HomePage(auth: auth);
           }
         }
         return Scaffold(
@@ -49,16 +31,5 @@ class _LandingPageState extends State<LandingPage> {
         );
       },
     );
-    // if (_user == null) {
-    //   return SignInPage(
-    //     auth: widget.auth,
-    //     onSignIn: _updateUser,
-    //   );
-    // } else {
-    //   return HomePage(
-    //     auth: widget.auth,
-    //     onSignOut: () => _updateUser(null),
-    //   );
-    // }
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User? get currentUser;
 
   Stream<User?> authStateChanges();
+
+  Future<User?> signInWithGoogle();
 
   Future<User?> signInAnonymously();
 
@@ -19,6 +22,24 @@ class Auth implements AuthBase {
   @override
   User? get currentUser => _firebaseAuth.currentUser;
 
+  Future<User?> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+            GoogleAuthProvider.credential(
+                idToken: googleAuth.idToken, accessToken: googleAuth.accessToken));
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(code: 'Error Missing Google Token', message: "Missing Token");
+      }
+    } else {
+      throw FirebaseAuthException(code: 'Error Aborted by User', message: "Sign In Aborted");
+    }
+  }
+
   @override
   Future<User?> signInAnonymously() async {
     final userCredentials = await _firebaseAuth.signInAnonymously();
@@ -27,6 +48,8 @@ class Auth implements AuthBase {
 
   @override
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
     await _firebaseAuth.signOut();
   }
 }
